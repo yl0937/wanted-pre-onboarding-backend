a73de4c71660cc0b2f1e2ccd7147b6b18f77ee62
yl0937 <yl1033371@gmail.com>
2023-08-09 12:12:45 +0900

    Feat: SignIn 기능 구현 * ErrorCode : 틀린 비밀번호, 없는 유저 ErrorCode * UserRepository : email로 유저 조회 * UserService : 이메일 조회 및 비밀번호 일치 확인

--- src/main/java/com/wanted/board/common/exception/ErrorCode.java
+++ src/main/java/com/wanted/board/common/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, 40001, "입력값 유효성 검사에 실패하였습니다."),
-    ALREADY_EXISTS_EMAIL(HttpStatus.BAD_REQUEST, 40002, "이미 가입된 이메일 입니다.");
+    ALREADY_EXISTS_EMAIL(HttpStatus.BAD_REQUEST, 40002, "이미 가입된 이메일 입니다."),
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, 40003, "비밀번호가 일치하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 40004, "사용자를 찾을 수 없습니다.");
+
 
     private final HttpStatus httpStatus;
     private final int code;
--- src/main/java/com/wanted/board/domain/user/service/UserService.java
+++ src/main/java/com/wanted/board/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.wanted.board.domain.user.service;
 
 import com.wanted.board.common.exception.BaseException;
 import com.wanted.board.common.exception.ErrorCode;
+import com.wanted.board.controller.dto.SignInRequest;
 import com.wanted.board.domain.user.entity.User;
 import com.wanted.board.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -32,5 +33,18 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    public Long signIn(SignInRequest signInRequest) {
+
+        User user = userRepository.findByEmail(signInRequest.getEmail())
+                .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+        String password = user.getPassword();
+
+        if (!passwordEncoder.matches(signInRequest.getPassword(), password)) {
+            throw new BaseException(ErrorCode.WRONG_PASSWORD);
+        }
+
+        return user.getId();
     }
 }
